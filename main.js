3aa7f511a0d61b6577b24256f1b57e88a0522b1e
Prince890840 <princepanchani890@gmail.com>
2023-02-19 10:59:00 +0530

    Select arrow Changes

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".mobile-navbar-container .close-icon");
 const mobileMenuContainer = document.querySelector(".mobile-navbar-container");
 
+const selectWrapper = document.querySelector('.select-wrapper');
+const select = selectWrapper.querySelector('select');
+const selectSpan = selectWrapper.querySelector('span');
+
 menuIcon.addEventListener("click", () => {
   mobileMenuContainer.classList.add("active");
 });
 
 closeIcon.addEventListener("click", () => {
   mobileMenuContainer.classList.remove("active");
+});
+
+selectWrapper.addEventListener('focusin', () => {
+  select.style.opacity = '1';
+  selectSpan.textContent = 'Select an option';
+});
+
+selectWrapper.addEventListener('focusout', () => {
+  select.style.opacity = '0';
+  selectSpan.textContent = select.value;
 });
