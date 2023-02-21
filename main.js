be0e3a780e4c7be11000e7a56923fc89f41ceff1
Prince890840 <princepanchani890@gmail.com>
2023-02-21 22:17:46 +0530

    Header Fixed and rest of the issue Fix

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const menuIcon = document.querySelector(".menu-icon");
-const closeIcon = document.querySelector(".mobile-navbar-container .close-icon");
+const closeIcon = document.querySelector(
+  ".mobile-navbar-container .close-icon"
+);
 const mobileMenuContainer = document.querySelector(".mobile-navbar-container");
 
-const selectWrapper = document.querySelector('.select-wrapper');
-const select = selectWrapper.querySelector('select');
-const selectSpan = selectWrapper.querySelector('span');
+const selectWrapper = document.querySelector(".select-wrapper");
+const select = selectWrapper.querySelector("select");
+const selectSpan = selectWrapper.querySelector("span");
+
+const announcementBar = document.querySelector(".announcementBar");
+const navbar = document.querySelector(".navbar");
+const smallScreenNavbar = document.querySelector(".mobile-nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > announcementBar.offsetHeight) {
+    navbar.classList.add("fixed");
+    smallScreenNavbar.classList.add("fixed");
+    announcementBar.style.display =   "none";
+  } else {
+    navbar.classList.remove("fixed");
+    smallScreenNavbar.classList.remove("fixed");
+    announcementBar.style.display = "block";
+  }
+});
 
 menuIcon.addEventListener("click", () => {
   mobileMenuContainer.classList.add("active");
@@ -14,12 +32,12 @@ closeIcon.addEventListener("click", () => {
   mobileMenuContainer.classList.remove("active");
 });
 
-selectWrapper.addEventListener('focusin', () => {
-  select.style.opacity = '1';
-  selectSpan.textContent = 'Select an option';
+selectWrapper.addEventListener("focusin", () => {
+  select.style.opacity = "1";
+  selectSpan.textContent = "Select an option";
 });
 
-selectWrapper.addEventListener('focusout', () => {
-  select.style.opacity = '0';
+selectWrapper.addEventListener("focusout", () => {
+  select.style.opacity = "0";
   selectSpan.textContent = select.value;
 });
